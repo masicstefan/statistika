8e707e54db26144f58bb284fa2cf4c8d0d0e984c
stefanblended <stefan.masic@um.si>
2023-01-29 16:25:28 +0100

    Zadnje spremembe

--- APIStatistika2App/Program.cs
+++ APIStatistika2App/Program.cs
@@ -63,16 +63,11 @@ builder.Services.AddAuthorization();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
-else
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+//}
 
 app.UseAuthentication();
 app.UseAuthorization();
